b7c443c222ee5bc1e03d08c786ab10c1a85c52b1
JamesMBartlett <jmabartlett@berkeley.edu>
2017-01-31 22:51:28 +0000

    debugging

--- seq_model.py
+++ seq_model.py
@@ -112,6 +112,7 @@ class SeqModel(Model):
             _, _, output_logits = self.model.step(self.sess, encoder_inputs, decoder_inputs,
                                              target_weights, bucket_id, True)
             # This is a greedy decoder - outputs are just argmaxes of output_logits.
+            print(output_logits)
             outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
             # If there is an EOS symbol in outputs, cut them at that point.
             if data_utils.EOS_ID in outputs:
